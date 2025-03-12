8d3a0aa7f69d2c9e897c72f3a035f00063424258
Chris Rowe <chris.rowe@wri.org>
2025-03-12 07:47:15 +0000

    Get all sessions

--- src/fetch_charging_sessions.py
+++ src/fetch_charging_sessions.py
@@ -1,11 +1,21 @@
 import time
 import os
+from python_chargepoint import ChargePoint
 
-def fetch_charging_sessions(client):
+
+def fetch_charging_sessions(pages):
+    session_list = []
+
+    # Set up ChargePoint
+    chargepoint_username = os.getenv('CHARGEPOINT_USERNAME')
+    chargepoint_password = os.getenv('CHARGEPOINT_PASSWORD')
+    client = ChargePoint(chargepoint_username, chargepoint_password)
+
+
     # Get the first 10 charging sessions
     response = client.session.get(f'{client.global_config.endpoints.mapcache}v2?{{"user_id":{client.user_id},"charging_activity_monthly":{{}}}}')
     # Get additional charging sessions in batches of 10
-    for i in range(1):
+    for i in range(pages):
         page_offset = response.json()["charging_activity_monthly"]["page_offset"]
         if page_offset == "last_page":
             break
@@ -14,13 +24,16 @@ def fetch_charging_sessions(client):
         req = f'{client.global_config.endpoints.mapcache}v2?{{"user_id":{client.user_id},"charging_activity_monthly":{{"page_offset":"{page_offset}"}}}}'
         response = client.session.get(req)
 
-        # Maybe not necessary to wait this long between requests?
-        time.sleep(5)
+        # Fetch charging sessions
+        month_info = response.json().get("charging_activity_monthly", {}).get("month_info", [])
     
-    return response
+        for sessions in month_info:
+            for session in sessions.get("sessions", {}):
+                session_list.append(session)
+
+        
+        # Maybe not necessary to wait this long between requests?
+        time.sleep(1)
 
 
-
-    {"charging_activity_monthly":{"page_size":20,"show_address_for_home_sessions":true}}
-    {"charging_activity_monthly":{"page_offset":"p_2025_1","page_size":20,"show_address_for_home_sessions":true}}
-    {"charging_activity_monthly":{"page_offset":"p_2024_12","page_size":20,"show_address_for_home_sessions":true}}
+    return session_list
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime, timedelta, UTC
-from python_chargepoint import ChargePoint
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from fetch_charging_sessions import fetch_charging_sessions
@@ -15,6 +14,7 @@ credentials = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 service = build('calendar', 'v3', credentials=credentials)
 
+
 def get_last_download_time():
     # Implement a way to store and retrieve the last download time
     # For example, use a file, database, or environment variable
@@ -24,59 +24,11 @@ def get_last_download_time():
     except FileNotFoundError:
         return datetime.now(UTC) - timedelta(days=1)
     
+
 def update_last_download_time(time):
     with open('last_download_time.txt', 'w') as file:
         file.write(time.isoformat())
-
-def get_sessions():
-    session_list = []
-
-    # Set up ChargePoint
-    chargepoint_username = os.getenv('CHARGEPOINT_USERNAME')
-    chargepoint_password = os.getenv('CHARGEPOINT_PASSWORD')
-    cp = ChargePoint(chargepoint_username, chargepoint_password)
-
-     # Fetch charging sessions
-    response = fetch_charging_sessions(cp)
-    month_info = response.json().get("charging_activity_monthly", {}).get("month_info", [])
     
-    for sessions in month_info:
-        for session in sessions.get("sessions", {}):
-            session_list.append(session)
-
-    return session_list
-
-def add_event_to_calendar(session):
-    current_charging = session['current_charging']
-    if current_charging != "done":
-        print("Session is still ongoing, skipping event creation.")
-    else:
-        from datetime import datetime
-        # Convert timestamp in milliseconds to datetime
-        start_time = datetime.fromtimestamp(session['start_time'] / 1000)
-        end_time = datetime.fromtimestamp(session['end_time'] / 1000)
-
-        session_time = session['session_time'] / 60000 / 60
-        energy_kwh = session['energy_kwh']
-        miles_added = session['miles_added']
-
-        session_description = f'{session_time:.2f} hour long session from {start_time} to {end_time}, Energy: {energy_kwh} kWh and {miles_added} miles added'
-
-        event = {
-            'summary': f'EV Charging Session: {energy_kwh} kWh',
-            'description': session_description,
-            'start': {
-                'dateTime': start_time.isoformat(),
-                'timeZone': 'UTC',
-            },
-            'end': {
-                'dateTime': end_time.isoformat(),
-                'timeZone': 'UTC',
-            },
-        }
-
-        event = service.events().insert(calendarId=calendar_id, body=event).execute()
-        print(f"Event created: {event.get('htmlLink')}")
 
 def get_or_create_calendar(service, calendar_name, user_email):
     # Check if the calendar exists
@@ -104,19 +56,54 @@ def get_or_create_calendar(service, calendar_name, user_email):
     
     return created_calendar['id']
 
+def add_event_to_calendar(session):
+    current_charging = session['current_charging']
+    if current_charging != "done":
+        print("Session is still ongoing, skipping event creation.")
+    else:
+        from datetime import datetime
+        # Convert timestamp in milliseconds to datetime
+        start_time = datetime.fromtimestamp(session['start_time'] / 1000)
+        end_time = datetime.fromtimestamp(session['end_time'] / 1000)
+
+        session_time = session['session_time'] / 60000 / 60
+        energy_kwh = session['energy_kwh']
+        miles_added = session['miles_added']
+
+        session_description = f'{session_time:.2f} hour long session from {start_time} to {end_time}, Energy: {energy_kwh} kWh and {miles_added} miles added'
+
+        event = {
+            'summary': f'EV Charging Session: {energy_kwh:.2f} kWh',
+            'description': session_description,
+            'start': {
+                'dateTime': start_time.isoformat(),
+                'timeZone': 'UTC',
+            },
+            'end': {
+                'dateTime': end_time.isoformat(),
+                'timeZone': 'UTC',
+            },
+        }
+
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        print(f"Event created: {event.get('htmlLink')}")
 
 def main():
     global calendar_id
-    print(user_email)
     calendar_id = get_or_create_calendar(service, "Chargepoint charging", user_email)
     print(f"Using calendar ID: {calendar_id}")
 
     last_download_time = get_last_download_time()
     print(f"Last download time: {last_download_time}")
 
-    session_descriptions = get_sessions()
+    sessions = fetch_charging_sessions(100)
+
+    # Save sessions to a file
+    with open('sessions.json', 'w') as f:
+        import json
+        json.dump(sessions, f)
  
-    for session in session_descriptions:
+    for session in sessions:
         # Uncomment the following line to add events to Google Calendar
         add_event_to_calendar(session)
 
@@ -124,5 +111,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
